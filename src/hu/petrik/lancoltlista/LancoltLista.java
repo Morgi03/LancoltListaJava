b1fd7ec6fff280608b4926746fa0aad6a78a8cc2
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-01 10:36:53 +0100

    hozzaAd

--- src/hu/petrik/lancoltlista/LancoltLista.java
+++ src/hu/petrik/lancoltlista/LancoltLista.java
@@ -1,8 +1,57 @@
 package hu.petrik.lancoltlista;
 
 public class LancoltLista<T> {
+    private ListaElem elso;
+    private ListaElem utolso;
+
+
+    public void hozzaAd(T ertek) {
+        if (this.elso == null) {
+            ListaElem ujElem = new ListaElem(ertek);
+            this.elso = ujElem;
+            this.utolso = ujElem;
+        } else {
+            ListaElem ujUtolso = new ListaElem(ertek);
+            ujUtolso.setElozo(this.utolso);
+            this.utolso.setKovetkezo(ujUtolso);
+            this.utolso = ujUtolso;
+        }
+
+    }
+
     private class ListaElem {
         private T ertek;
         private ListaElem kovetkezo;
+        private ListaElem elozo;
+
+        public ListaElem(T ertek) {
+            this.ertek = ertek;
+            this.kovetkezo = null;
+            this.elozo = null;
+        }
+
+        public ListaElem getKovetkezo() {
+            return kovetkezo;
+        }
+
+        public void setKovetkezo(ListaElem kovetkezo) {
+            this.kovetkezo = kovetkezo;
+        }
+
+        public ListaElem getElozo() {
+            return elozo;
+        }
+
+        public void setElozo(ListaElem elozo) {
+            this.elozo = elozo;
+        }
+
+        public void hozzaAd(T ertek) {
+            if (this.kovetkezo == null) {
+                this.kovetkezo = new ListaElem(ertek);
+            } else {
+                this.kovetkezo.hozzaAd(ertek);
+            }
+        }
     }
 }
--- src/hu/petrik/lancoltlista/Main.java
+++ src/hu/petrik/lancoltlista/Main.java
@@ -2,6 +2,14 @@ package hu.petrik.lancoltlista;
 
 public class Main {
     public static void main(String[] args) {
+        LancoltLista<Integer> lista = new LancoltLista<>();
+        int elemekSzama = 1200000;
+        int legkisebb = 1;
+        int legnagyobb = 420;
+        for (int i = 0; i < elemekSzama; i++) {
+            int ertek = (int) (Math.random() * (legnagyobb - legkisebb + 1));
+            lista.hozzaAd(ertek);
+        }
 
     }
 }
